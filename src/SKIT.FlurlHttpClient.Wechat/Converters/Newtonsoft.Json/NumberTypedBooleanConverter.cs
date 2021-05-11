94d14f83c5e760fe947d81ee3b0cdceaf890de25
Fu Diwei <fudiwei@sina.com>
2021-05-12 01:04:42 +0800

    feat(core): 新增若干 bool 非标准化 JsonConvert

---
+++ src/SKIT.FlurlHttpClient.Wechat/Converters/Newtonsoft.Json/NumberTypedBooleanConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Newtonsoft.Json.Converters
+{
+    public class NumberTypedBooleanConverter : JsonConverter<bool>
+    {
+        private readonly JsonConverter<bool?> _converter = new NumberTypedNullableBooleanConverter();
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        public override bool ReadJson(JsonReader reader, Type objectType, bool existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            return _converter.ReadJson(reader, objectType, existingValue, hasExistingValue, serializer) ?? default;
+        }
+
+        public override void WriteJson(JsonWriter writer, bool value, JsonSerializer serializer)
+        {
+            _converter.WriteJson(writer, value, serializer);
+        }
+    }
+}
