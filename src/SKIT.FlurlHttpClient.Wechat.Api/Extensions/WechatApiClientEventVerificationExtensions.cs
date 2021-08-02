f35ed2abf368ab524f6c8c2fe2b5b648be22b530
Fu Diwei <fudiwei@sina.com>
2021-08-02 16:15:00 +0800

    feat(wxapi): 新增验证微信回调通知事件签名的扩展方法

---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Extensions/WechatApiClientEventVerificationExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SKIT.FlurlHttpClient.Wechat.Api
+{
+    /// <summary>
+    /// 为 <see cref="WechatApiClient"/> 提供回调通知事件验证的扩展方法。
+    /// </summary>
+    public static class WechatApiClientEventVerificationExtensions
+    {
+        /// <summary>
+        /// <para>验证回调通知事件签名。</para>
+        /// <para>REF: https://developers.weixin.qq.com/doc/offiaccount/Basic_Information/Access_Overview.html </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="callbackTimestamp">微信回调通知中的 timestamp 字段。</param>
+        /// <param name="callbackNonce">微信回调通知中的 nonce 字段。</param>
+        /// <param name="callbackSignature">微信回调通知中的 signature 字段。</param>
+        /// <returns></returns>
+        public static bool VerifyEventSignature(
+            this WechatApiClient client,
+            string callbackTimestamp,
+            string callbackNonce,
+            string callbackSignature)
+        {
+            if (client == null) throw new ArgumentNullException(nameof(client));
+            if (callbackTimestamp == null) throw new ArgumentNullException(nameof(callbackTimestamp));
+            if (callbackNonce == null) throw new ArgumentNullException(nameof(callbackNonce));
+            if (callbackSignature == null) throw new ArgumentNullException(nameof(callbackSignature));
+
+            ISet<string> set = new SortedSet<string>() { client.Credentials.PushToken!, callbackTimestamp, callbackNonce };
+            string sign = Security.SHA1Utility.Hash(string.Concat(set));
+            return string.Equals(sign, callbackSignature, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
