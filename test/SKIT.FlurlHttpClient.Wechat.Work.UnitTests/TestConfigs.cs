677c04898d40f45932f2b4b4f50ef8dc10cce678
Fu Diwei <fudiwei@sina.com>
2021-08-05 17:57:13 +0800

    test: 调整测试项目配置

--- test/SKIT.FlurlHttpClient.Wechat.Work.UnitTests/TestConfigs.cs
+++ test/SKIT.FlurlHttpClient.Wechat.Work.UnitTests/TestConfigs.cs
@@ -8,10 +8,10 @@ namespace SKIT.FlurlHttpClient.Wechat.Work.UnitTests
     {
         static TestConfigs()
         {
-            // NOTICE: 请在项目根目录下建立 appsettings.local.json，按照 appsettings.json 的格式填入测试参数。
+            // NOTICE: 请在项目根目录下按照 appsettings.json 的格式填入测试参数。
             // WARN: 敏感信息请不要提交到 git！
 
-            using var stream = File.OpenRead("appsettings.local.json");
+            using var stream = File.OpenRead("appsettings.json");
             using var json = JsonDocument.Parse(stream);
 
             var config = json.RootElement.GetProperty("WechatConfig");
