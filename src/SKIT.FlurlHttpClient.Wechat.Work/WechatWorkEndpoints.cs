217be0f7c60107b673b031944823c3a05a501a1f
Fu Diwei <fudiwei@sina.com>
2021-08-11 17:46:25 +0800

    style: clean code

--- src/SKIT.FlurlHttpClient.Wechat.Work/WechatWorkEndpoints.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Work/WechatWorkEndpoints.cs
@@ -4,7 +4,6 @@ namespace SKIT.FlurlHttpClient.Wechat.Work
 {
     /// <summary>
     /// <para>企业微信 API 接口域名。</para>
-    /// <para>REF: https://developers.weixin.qq.com/doc/offiaccount/Basic_Information/Interface_field_description.html </para>
     /// </summary>
     public static class WechatWorkEndpoints
     {
