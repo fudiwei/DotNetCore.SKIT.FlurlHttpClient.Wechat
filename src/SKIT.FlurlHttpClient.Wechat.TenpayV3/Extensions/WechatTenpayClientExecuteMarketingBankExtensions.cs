301c81b770a4d6bfb02d932b6e5fe2a747a90591
Fu Diwei <fudiwei@sina.com>
2021-08-17 14:27:02 +0800

    feat(tenpayv3): 新增银行定向促活相关接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.TenpayV3/Extensions/WechatTenpayClientExecuteMarketingBankExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Flurl;
+using Flurl.Http;
+
+namespace SKIT.FlurlHttpClient.Wechat.TenpayV3
+{
+    /// <summary>
+    /// 为 <see cref="WechatTenpayClient"/> 提供银行定向促活相关的 API 扩展方法。
+    /// </summary>
+    public static class WechatTenpayClientExecuteMarketingBankExtensions
+    {
+        /// <summary>
+        /// <para>异步调用 [POST] /marketing/bank/packages/{package_id}/tasks 接口。</para>
+        /// <para>REF: https://pay.weixin.qq.com/wiki/doc/apiv3/apis/chapter9_8_1.shtml </para>
+        /// <para>REF: https://pay.weixin.qq.com/wiki/doc/apiv3_partner/apis/chapter9_8_1.shtml </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.UploadMarketingBankPackagesTasksResponse> ExecuteUploadMarketingBankPackagesTasksAsync(this WechatTenpayClient client, Models.UploadMarketingBankPackagesTasksRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            if (string.IsNullOrEmpty(request.FileName))
+                request.FileName = Guid.NewGuid().ToString("N").ToLower() + ".txt";
+
+            if (string.IsNullOrEmpty(request.FileHash))
+                request.FileHash = Security.SHA256Utility.Hash(request.FileBytes).ToLower();
+
+            if (string.IsNullOrEmpty(request.FileContentType))
+                request.FileContentType = Utilities.FileNameToContentTypeMapper.GetContentTypeForImage(request.FileName!) ?? "text/plain";
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "marketing", "bank", "packages", request.PackageId, "tasks");
+
+            string boundary = "--BOUNDARY--" + DateTimeOffset.Now.Ticks.ToString("x");
+            using var fileContent = new ByteArrayContent(request.FileBytes);
+            using var metaContent = new ByteArrayContent(Encoding.UTF8.GetBytes(client.JsonSerializer.Serialize(request)));
+            using var httpContent = new MultipartFormDataContent(boundary);
+            httpContent.Add(metaContent, $"\"{Constants.FormDataFields.FORMDATA_META}\"");
+            httpContent.Add(fileContent, "\"file\"", $"\"{HttpUtility.UrlEncode(request.FileName)}\"");
+            httpContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data; boundary=" + boundary);
+            metaContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse(request.FileContentType);
+
+            return await client.SendRequestAsync<Models.UploadMarketingBankPackagesTasksResponse>(flurlReq, httpContent: httpContent, cancellationToken: cancellationToken);
+        }
+    }
+}
