fb791b07a771d8a724b06bc3d827f408b09f4ee3
fudiwei <fudiwei@sina.com>
2021-09-03 14:23:19 +0800

    feat(wxapi): 新增检查加密信息是否由微信生成的接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/WxaBusiness/WxaBusinessCheckEncryptedMessageResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.Wechat.Api.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /wxa/business/checkencryptedmsg 接口的响应。</para>
+    /// </summary>
+    public class WxaBusinessCheckEncryptedMessageResponse : WechatApiResponse
+    {
+        /// <summary>
+        /// 获取或设置是否是合法的数据。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("vaild")]
+        [System.Text.Json.Serialization.JsonPropertyName("vaild")]
+        public bool IsValid { get; set; }
+
+        /// <summary>
+        /// 获取或设置加密数据生成的时间戳。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("create_time")]
+        [System.Text.Json.Serialization.JsonPropertyName("create_time")]
+        public long CreateTimestamp { get; set; }
+    }
+}
