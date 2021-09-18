12034895a69b0647ce3ad496e53f3c6b420e3b89
Fu Diwei <fudiwei@sina.com>
2021-09-18 11:55:13 +0800

    feat(wxapi): 新增微信红包封面相关接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Extensions/WechatApiClientExecuteRedPacketCoverExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace SKIT.FlurlHttpClient.Wechat.Api
+{
+    public static class WechatApiClientExecuteRedPacketCoverExtensions
+    {
+        /// <summary>
+        /// <para>异步调用 [POST] /redpacketcover/wxapp/cover_url/get_by_token 接口。</para>
+        /// <para>REF: https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/redpacketcover/redpacketcover.getAuthenticationUrl.html </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.RedPacketCoverWxAppCoverUrlGetByTokenResponse> ExecuteRedPacketCoverWxAppCoverUrlGetByTokenAsync(this WechatApiClient client, Models.RedPacketCoverWxAppCoverUrlGetByTokenRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "redpacketcover", "wxapp", "cover_url", "get_by_token")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.RedPacketCoverWxAppCoverUrlGetByTokenResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/RedPacketCover/RedPacketCoverWxAppCoverUrlGetByTokenResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.Wechat.Api.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /redpacketcover/wxapp/cover_url/get_by_token 接口的响应。</para>
+    /// </summary>
+    public class RedPacketCoverWxAppCoverUrlGetByTokenResponse : WechatApiResponse
+    {
+        public static class Types
+        {
+            public class Data
+            {
+                /// <summary>
+                /// 获取或设置用户领取的链接。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("url")]
+                [System.Text.Json.Serialization.JsonPropertyName("url")]
+                public string Url { get; set; } = default!;
+            }
+        }
+
+        /// <summary>
+        /// 获取或设置返回数据。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("data")]
+        [System.Text.Json.Serialization.JsonPropertyName("data")]
+        public Types.Data Data { get; set; } = default!;
+    }
+}
