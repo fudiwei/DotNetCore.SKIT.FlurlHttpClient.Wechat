b808fc63abc37c23841ea0a6f4391c1177e9d89e
Fu Diwei <fudiwei@sina.com>
2021-09-18 14:58:55 +0800

    feat(wxapi): 新增用户领券相关的回调通知事件模型

---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Events/OpenProduct/OpenProductReceiveCouponEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.Wechat.Api.Events
+{
+    /// <summary>
+    /// <para>表示 EVENT.open_product_receive_coupon 事件的数据。</para>
+    /// <para>REF: https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/business-capabilities/ministore/minishopopencomponent2/callback/receive_coupon.html </para>
+    /// </summary>
+    public class OpenProductReceiveCouponEvent : WechatApiEvent, WechatApiEvent.Serialization.IJsonSerializable, WechatApiEvent.Serialization.IXmlSerializable
+    {
+        /// <summary>
+        /// 获取或设置商家侧优惠券 ID。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("out_coupon_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("out_coupon_id")]
+        [System.Xml.Serialization.XmlElement("out_coupon_id")]
+        public string OutCouponId { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置请求唯一标识。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("request_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("request_id")]
+        [System.Xml.Serialization.XmlElement("request_id")]
+        public string RequestId { get; set; } = default!;
+    }
+}
