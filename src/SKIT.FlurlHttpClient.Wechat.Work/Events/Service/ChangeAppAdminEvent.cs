c4c6e2e3042670f83dc22113ff8eed1b927b4a9c
Fu Diwei <fudiwei@sina.com>
2021-09-30 18:20:25 +0800

    feat(work): 新增管理员变更相关的回调通知事件模型

---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Events/Service/ChangeAppAdminEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Events
+{
+    /// <summary>
+    /// <para>表示 EVENT.change_app_admin 事件的数据。</para>
+    /// <para>REF: https://open.work.weixin.qq.com/api/doc/90001/90143/95038 </para>
+    /// </summary>
+    public class ChangeAppAdminEvent : WechatWorkEvent, WechatWorkEvent.Types.IXmlSerializable
+    {
+        /// <summary>
+        /// 获取或设置应用 ID。
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("AgentID")]
+        public int AgentId { get; set; }
+    }
+}
