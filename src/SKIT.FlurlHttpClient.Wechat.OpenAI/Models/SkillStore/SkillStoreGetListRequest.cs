ec10df05e6541afc26b51779c660cb610fd628f5
Fu Diwei <fudiwei@sina.com>
2021-10-08 16:28:59 +0800

    feat(openai): 新增技能商店相关接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.OpenAI/Models/SkillStore/SkillStoreGetListRequest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.Wechat.OpenAI.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /v2/skill_store/get_list 接口的请求。</para>
+    /// </summary>
+    public class SkillStoreGetListRequest : WechatOpenAIRequest
+    {
+        /// <summary>
+        /// 获取或设置关键字。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("keyword")]
+        [System.Text.Json.Serialization.JsonPropertyName("keyword")]
+        public string? Keyword { get; set; }
+
+        /// <summary>
+        /// 获取或设置技能类别。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("category")]
+        [System.Text.Json.Serialization.JsonPropertyName("category")]
+        public string? Category { get; set; }
+
+        /// <summary>
+        /// 获取或设置分类字段。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("order")]
+        [System.Text.Json.Serialization.JsonPropertyName("order")]
+        public int Order { get; set; }
+
+        /// <summary>
+        /// 获取或设置分页页数。
+        /// <para>默认值：1</para>
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("page")]
+        [System.Text.Json.Serialization.JsonPropertyName("page")]
+        public int Page { get; set; } = 1;
+
+        /// <summary>
+        /// 获取或设置分页每页数量。
+        /// <para>默认值：10</para>
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("size")]
+        [System.Text.Json.Serialization.JsonPropertyName("size")]
+        public int Limit { get; set; } = 10;
+    }
+}
