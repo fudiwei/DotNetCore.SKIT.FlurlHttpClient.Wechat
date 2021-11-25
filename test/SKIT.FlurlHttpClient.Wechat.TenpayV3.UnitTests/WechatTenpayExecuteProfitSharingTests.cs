d0d1d6ceb5d34cd6228ec6ad88b3d023ba7d7dd6
Fu Diwei <fudiwei@sina.com>
2021-11-26 01:44:30 +0800

    test(tenpayv3): 补充添加分账接收方接口的测试用例

---
+++ test/SKIT.FlurlHttpClient.Wechat.TenpayV3.UnitTests/WechatTenpayExecuteProfitSharingTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SKIT.FlurlHttpClient.Wechat.TenpayV3.UnitTests
+{
+    public class WechatTenpayExecuteProfitSharingTests
+    {
+        [Fact(DisplayName = "[POST] /profitsharing/receivers/add")]
+        public async Task AddProfitSharingReceiverTest()
+        {
+            var cert = TestClients.Instance.DecryptResponseSensitiveProperty(await TestClients.Instance.ExecuteQueryCertificatesAsync(new Models.QueryCertificatesRequest()));
+            foreach (var certificateModel in cert.CertificateList)
+            {
+                TestClients.GlobalCertificateManager.AddEntry(new Settings.CertificateEntry(certificateModel));
+            }
+
+            var request = new Models.AddProfitSharingReceiverRequest()
+            {
+                AppId = TestConfigs.WechatAppId,
+                Account = TestConfigs.WechatOpenId,
+                RelationType = "DISTRIBUTOR",
+                Name = "NAME",
+                Type = "PERSONAL_OPENID"
+            };
+            TestClients.Instance.EncryptRequestSensitiveProperty(request);
+            var response = await TestClients.Instance.ExecuteAddProfitSharingReceiverAsync(request);
+
+            Assert.True(response.IsSuccessful());
+        }
+    }
+}
