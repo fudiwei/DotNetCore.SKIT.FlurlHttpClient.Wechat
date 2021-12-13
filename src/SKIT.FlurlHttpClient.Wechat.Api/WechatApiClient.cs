7b0c793ae71384a5a5aba163c6272374f1b642f0
Fu Diwei <fudiwei@sina.com>
2021-12-13 17:13:25 +0800

    fix: 修复空响应时无法正确反序列化的问题

--- src/SKIT.FlurlHttpClient.Wechat.Api/WechatApiClient.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Api/WechatApiClient.cs
@@ -111,9 +111,9 @@ namespace SKIT.FlurlHttpClient.Wechat.Api
             where T : WechatApiResponse, new()
         {
             byte[] bytes = await flurlResponse.GetBytesAsync().ConfigureAwait(false);
-            bool jsonable = bytes.Length > 1 &&
-                (bytes[0] == 91 && bytes[bytes.Length - 1] == 93) || // "[...]"
-                (bytes[0] == 123 && bytes[bytes.Length - 1] == 125); // "{...}"
+            bool jsonable =
+                (bytes.Length > 1 && bytes[0] == 91 && bytes[bytes.Length - 1] == 93) || // "[...]"
+                (bytes.Length > 1 && bytes[0] == 123 && bytes[bytes.Length - 1] == 125); // "{...}"
 
             T result = jsonable ? JsonSerializer.Deserialize<T>(Encoding.UTF8.GetString(bytes)) : new T();
 
--- src/SKIT.FlurlHttpClient.Wechat.OpenAI/WechatOpenAIThirdPartyClient.cs
+++ src/SKIT.FlurlHttpClient.Wechat.OpenAI/WechatOpenAIThirdPartyClient.cs
@@ -128,9 +128,9 @@ namespace SKIT.FlurlHttpClient.Wechat.OpenAI
             where T : WechatOpenAIThirdPartyResponse, new()
         {
             byte[] bytes = await flurlResponse.GetBytesAsync().ConfigureAwait(false);
-            bool jsonable = bytes.Length > 1 &&
-                (bytes[0] == 91 && bytes[bytes.Length - 1] == 93) || // "[...]"
-                (bytes[0] == 123 && bytes[bytes.Length - 1] == 125); // "{...}"
+            bool jsonable =
+                (bytes.Length > 1 && bytes[0] == 91 && bytes[bytes.Length - 1] == 93) || // "[...]"
+                (bytes.Length > 1 && bytes[0] == 123 && bytes[bytes.Length - 1] == 125); // "{...}"
 
             T result = jsonable ? JsonSerializer.Deserialize<T>(Encoding.UTF8.GetString(bytes)) : new T();
 
