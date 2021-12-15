86aa5ef01d5ef33c73321ad45278bd58c006b5bd
Fu Diwei <fudiwei@sina.com>
2021-12-15 15:06:12 +0800

    fix(tenpayv3): fix typo

--- src/SKIT.FlurlHttpClient.Wechat.TenpayV3/Constants/EncryptionAlgorithms.cs
+++ src/SKIT.FlurlHttpClient.Wechat.TenpayV3/Constants/EncryptionAlgorithms.cs
@@ -4,6 +4,6 @@
     {
         public const string AEAD_AES_256_GCM = "AEAD_AES_256_GCM";
 
-        public const string RSA_2048_PKCS8_ECB = "RSA_2048_PKCS18_ECB";
+        public const string RSA_2048_PKCS8_ECB = "RSA_2048_PKCS8_ECB";
     }
 }
