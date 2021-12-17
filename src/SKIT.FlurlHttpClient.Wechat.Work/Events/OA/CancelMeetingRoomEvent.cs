f51a4a4d66cdda3b34f87d063b7169f2e818f16d
Fu Diwei <fudiwei@sina.com>
2021-12-17 21:14:09 +0800

    feat(work): 新增会议室预定的回调通知事件模型

---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Events/OA/CancelMeetingRoomEvent.cs
@@ -0,0 +1,25 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Events
+{
+    /// <summary>
+    /// <para>表示 EVENT.cancel_meeting_room 事件的数据。</para>
+    /// <para>REF: https://open.work.weixin.qq.com/api/doc/90000/90135/95333 </para>
+    /// </summary>
+    public class CancelMeetingRoomEvent : WechatWorkEvent, WechatWorkEvent.Serialization.IJsonSerializable, WechatWorkEvent.Serialization.IXmlSerializable
+    {
+        /// <summary>
+        /// 获取或设置会议室 ID。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("MeetingRoomId")]
+        [System.Text.Json.Serialization.JsonPropertyName("MeetingRoomId")]
+        [System.Xml.Serialization.XmlElement("MeetingRoomId")]
+        public int MeetingRoomId { get; set; } 
+
+        /// <summary>
+        /// 获取或设置会议 ID。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("MeetingId")]
+        [System.Text.Json.Serialization.JsonPropertyName("MeetingId")]
+        [System.Xml.Serialization.XmlElement("MeetingId")]
+        public string MeetingId { get; set; } = default!;
+    }
+}
