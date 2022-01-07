2f300f45f9be403d3127881a56e0987b70bd8b90
Fu Diwei <fudiwei@sina.com>
2022-01-07 13:28:11 +0800

    feat(tenpayv3): 新增银行组件相关接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.TenpayV3/Models/Capital/Banks/QueryCapitalBanksBranchesRequest.cs
@@ -0,0 +1,36 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.TenpayV3.Models
+{
+    /// <summary>
+    /// <para>表示 [GET] /capital/capitallhh/banks/{bank_alias_code}/branches 接口的请求。</para>
+    /// </summary>
+    public class QueryCapitalBanksBranchesRequest : WechatTenpayRequest
+    {
+        /// <summary>
+        /// 获取或设置银行别名编码。
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public string BankAliasCode { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 获取或设置城市编码。
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public int CityCode { get; set; }
+
+        /// <summary>
+        /// 获取或设置分页大小。
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public int Limit { get; set; } = 10;
+
+        /// <summary>
+        /// 获取或设置分页开始位置。
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public int Offset { get; set; }
+    }
+}
