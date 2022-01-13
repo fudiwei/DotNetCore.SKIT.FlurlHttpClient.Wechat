d808721c9938c446f7bc903034a903ffbdd1f0a2
Fu Diwei <fudiwei@sina.com>
2022-01-13 17:59:08 +0800

    feat(work): 新增上下游相关接口

--- src/SKIT.FlurlHttpClient.Wechat.Work/Extensions/WechatWorkClientExecuteCgibinCorpGroupExtensions.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Extensions/WechatWorkClientExecuteCgibinCorpGroupExtensions.cs
@@ -52,5 +52,92 @@ namespace SKIT.FlurlHttpClient.Wechat.Work
 
             return await client.SendRequestWithJsonAsync<Models.CgibinCorpGroupCropGetTokenResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
         }
+
+        /// <summary>
+        /// <para>异步调用 [GET] /cgi-bin/corpgroup/corp/get_chain_list 接口。</para>
+        /// <para>REF: https://developer.work.weixin.qq.com/document/path/95315 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.CgibinCorpGroupCropGetChainListResponse> ExecuteCgibinCorpGroupCropGetChainListAsync(this WechatWorkClient client, Models.CgibinCorpGroupCropGetChainListRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Get, "cgi-bin", "corpgroup", "corp", "get_chain_list")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.CgibinCorpGroupCropGetChainListResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// <para>异步调用 [POST] /cgi-bin/corpgroup/corp/get_chain_group 接口。</para>
+        /// <para>REF: https://developer.work.weixin.qq.com/document/path/95315 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.CgibinCorpGroupCropGetChainGroupResponse> ExecuteCgibinCorpGroupCropGetChainGroupAsync(this WechatWorkClient client, Models.CgibinCorpGroupCropGetChainGroupRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "cgi-bin", "corpgroup", "corp", "get_chain_group")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.CgibinCorpGroupCropGetChainGroupResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// <para>异步调用 [GET] /cgi-bin/corpgroup/corp/get_chain_corpinfo_list 接口。</para>
+        /// <para>REF: https://developer.work.weixin.qq.com/document/path/95315 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.CgibinCorpGroupCropGetChainCorpInfoListResponse> ExecuteCgibinCorpGroupCropGetChainCorpInfoListAsync(this WechatWorkClient client, Models.CgibinCorpGroupCropGetChainCorpInfoListRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Get, "cgi-bin", "corpgroup", "corp", "get_chain_corpinfo_list")
+                .SetQueryParam("access_token", request.AccessToken)
+                .SetQueryParam("chain_id", request.ChainId);
+
+            if (request.GroupId != null)
+                flurlReq.SetQueryParam("groupid", request.GroupId.Value);
+
+            if (request.RequireFetchChild != null)
+                flurlReq.SetQueryParam("fetch_child", request.RequireFetchChild.Value ? 1 : 0);
+
+            return await client.SendRequestWithJsonAsync<Models.CgibinCorpGroupCropGetChainCorpInfoListResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// <para>异步调用 [POST] /cgi-bin/corpgroup/unionid_to_external_userid 接口。</para>
+        /// <para>REF: https://developer.work.weixin.qq.com/document/path/95342 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.CgibinCorpGroupUnionidToExternalUserIdResponse> ExecuteCgibinCorpGroupUnionidToExternalUserIdAsync(this WechatWorkClient client, Models.CgibinCorpGroupUnionidToExternalUserIdRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "cgi-bin", "corpgroup", "unionid_to_external_userid")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.CgibinCorpGroupUnionidToExternalUserIdResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
     }
 }
---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinCorpGroup/CgibinCorpGroupCropGetChainCorpInfoListResponse.cs
@@ -0,0 +1,49 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Models
+{
+    /// <summary>
+    /// <para>表示 [GET] /cgi-bin/corpgroup/corp/get_chain_corpinfo_list 接口的响应。</para>
+    /// </summary>
+    public class CgibinCorpGroupCropGetChainCorpInfoListResponse : WechatWorkResponse
+    {
+        public static class Types
+        {
+            public class GroupCorp
+            {
+                /// <summary>
+                /// 获取或设置分组 ID。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("groupid")]
+                [System.Text.Json.Serialization.JsonPropertyName("groupid")]
+                public int GroupId { get; set; }
+
+                /// <summary>
+                /// 获取或设置企业 CorpId。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("corpid")]
+                [System.Text.Json.Serialization.JsonPropertyName("corpid")]
+                public string CorpId { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置企业名称。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("corp_name")]
+                [System.Text.Json.Serialization.JsonPropertyName("corp_name")]
+                public string CorpName { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置自定义 ID。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("custom_id")]
+                [System.Text.Json.Serialization.JsonPropertyName("custom_id")]
+                public string? CustomId { get; set; }
+            }
+        }
+
+        /// <summary>
+        /// 获取或设置企业上下游通讯录分组下的企业详情列表。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("group_corps")]
+        [System.Text.Json.Serialization.JsonPropertyName("group_corps")]
+        public Types.GroupCorp[] GroupCorpList { get; set; } = default!;
+    }
+}
