f8311519ffa123233d913968699bfeee4fbf4141
Fu Diwei <fudiwei@sina.com>
2022-01-16 17:38:33 +0800

    feat(tenpayv2): 新增根据付款码查询 OpenId 相关接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.TenpayV2/Models/Tools/ToolsAuthCodeToOpenIdRequest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SKIT.FlurlHttpClient.Wechat.TenpayV2.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /tools/authcodetoopenid 接口的请求。</para>
+    /// </summary>
+    public class ToolsAuthCodeToOpenIdRequest : WechatTenpaySignableRequest
+    {
+        /// <summary>
+        /// 获取或设置微信商户号。如果不指定将使用构造 <see cref="WechatTenpayClient"/> 时的 <see cref="WechatTenpayClientOptions.MerchantId"/> 参数。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("mch_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("mch_id")]
+        public string? MerchantId { get; set; }
+
+        /// <summary>
+        /// 获取或设置子商户号。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("sub_mch_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("sub_mch_id")]
+        public string? SubMerchantId { get; set; }
+
+        /// <summary>
+        /// 获取或设置微信 AppId。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("appid")]
+        [System.Text.Json.Serialization.JsonPropertyName("appid")]
+        public string AppId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 获取或设置子商户 AppId。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("sub_appid")]
+        [System.Text.Json.Serialization.JsonPropertyName("sub_appid")]
+        public string? SubAppId { get; set; }
+
+        /// <summary>
+        /// 获取或设置付款码。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("auth_code")]
+        [System.Text.Json.Serialization.JsonPropertyName("auth_code")]
+        public string AuthCode { get; set; } = string.Empty;
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.Wechat.TenpayV2/Models/Tools/ToolsAuthCodeToOpenIdResponse.cs
@@ -0,0 +1,62 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.TenpayV2.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /tools/authcodetoopenid 接口的响应。</para>
+    /// </summary>
+    public class ToolsAuthCodeToOpenIdResponse : WechatTenpaySignableResponse
+    {
+        /// <summary>
+        /// 获取或设置业务结果。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("result_code")]
+        [System.Text.Json.Serialization.JsonPropertyName("result_code")]
+        public string ResultCode { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置微信商户号。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("mch_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("mch_id")]
+        public string MerchantId { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置子商户号。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("sub_mch_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("sub_mch_id")]
+        public string? SubMerchantId { get; set; }
+
+        /// <summary>
+        /// 获取或设置微信 AppId。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("appid")]
+        [System.Text.Json.Serialization.JsonPropertyName("appid")]
+        public string AppId { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置子商户 AppId。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("sub_appid")]
+        [System.Text.Json.Serialization.JsonPropertyName("sub_appid")]
+        public string? SubAppId { get; set; }
+
+        /// <summary>
+        /// 获取或设置用户唯一标识。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("openid")]
+        [System.Text.Json.Serialization.JsonPropertyName("openid")]
+        public string OpenId { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置用户在子商户下的唯一标识。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("sub_openid")]
+        [System.Text.Json.Serialization.JsonPropertyName("sub_openid")]
+        public string? SubOpenId { get; set; }
+
+        public override bool IsSuccessful()
+        {
+            return base.IsSuccessful() && "SUCCESS".Equals(ResultCode);
+        }
+    }
+}
