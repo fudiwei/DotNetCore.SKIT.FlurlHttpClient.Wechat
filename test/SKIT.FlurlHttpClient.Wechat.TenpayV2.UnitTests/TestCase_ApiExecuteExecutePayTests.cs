bd6f72d1cc439b50387e515137273ddd3b7bdf95
Fu Diwei <fudiwei@sina.com>
2022-01-23 17:39:02 +0800

    test(tenpayv2): 增加付款码付款的单元测试用例

---
+++ test/SKIT.FlurlHttpClient.Wechat.TenpayV2.UnitTests/TestCase_ApiExecuteExecutePayTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SKIT.FlurlHttpClient.Wechat.TenpayV2.UnitTests
+{
+    public class TestCase_ApiExecuteExecutePayTransactionsTests
+    {
+        [Fact(DisplayName = "测试用例：调用 API [POST] /pay/micropay")]
+        public async Task TestCase_ApiExecuteExecutePayTests()
+        {
+            var request = new Models.CreatePayMicroPayRequest()
+            {
+                OutTradeNumber = "TEST_OTN_" + DateTimeOffset.Now.ToString("yyyyMMddHHmmssfff"),
+                Body = "TEST_BODY",
+                TotalFee = 1,
+                ClientIp = "127.0.0.7",
+                AuthCode = "TEST_AUTH_CODE"
+            };
+            var response = await TestClients.Instance.ExecuteCreatePayMicroPayAsync(request);
+
+            Assert.NotNull(response.TransactionId);
+        }
+    }
+}
