62f242a714957376d2cf196457321de4b38d2d10
Qin Yikai <QYK_zZ@hotmail.com>
2022-04-19 19:14:54 +0800

    feat(wxapi): 新增自定义交易组件订单回调通知事件模型
    * feat(wxapi): 新增自定义交易组件订单回调通知事件模型
    
    * fix(wxapi): 修复自定义交易组件订单发货接口请求模型定义问题
    
    * fix(wxapi): 更新自定义交易组件售后列表接口响应模型定义问题

--- src/SKIT.FlurlHttpClient.Wechat.Api/Models/Shop/ECAftersale/ShopECAftersaleGetResponse.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/Shop/ECAftersale/ShopECAftersaleGetResponse.cs
@@ -7,8 +7,159 @@
     {
         public static class Types
         {
-            public class AftersaleOrder : ShopECAftersaleGetListResponse.Types.AftersaleOrder
+            public class AftersaleOrder
             {
+                public static class Types
+                {
+                    public class Product
+                    {
+                        /// <summary>
+                        /// 获取或设置商家自定义商品 ID。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("out_product_id")]
+                        [System.Text.Json.Serialization.JsonPropertyName("out_product_id")]
+                        public string? OutProductId { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置商家自定义 SKU ID。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("out_sku_id")]
+                        [System.Text.Json.Serialization.JsonPropertyName("out_sku_id")]
+                        public string? OutSKUId { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置参与售后的商品数量。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("product_cnt")]
+                        [System.Text.Json.Serialization.JsonPropertyName("product_cnt")]
+                        public int Count { get; set; }
+                    }
+
+                    public class Return
+                    {
+                        /// <summary>
+                        /// 获取或设置快递公司 ID。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("delivery_id")]
+                        [System.Text.Json.Serialization.JsonPropertyName("delivery_id")]
+                        public string DeliveryId { get; set; } = default!;
+
+                        /// <summary>
+                        /// 获取或设置快递公司名字。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("delivery_name")]
+                        [System.Text.Json.Serialization.JsonPropertyName("delivery_name")]
+                        public string DeliveryName { get; set; } = default!;
+
+                        /// <summary>
+                        /// 获取或设置快递单号。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("waybill_id")]
+                        [System.Text.Json.Serialization.JsonPropertyName("waybill_id")]
+                        public string WaybillId { get; set; } = default!;
+
+                        /// <summary>
+                        /// 获取或设置退货时间戳。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("order_return_time")]
+                        [System.Text.Json.Serialization.JsonPropertyName("order_return_time")]
+                        [System.Text.Json.Serialization.JsonNumberHandling(System.Text.Json.Serialization.JsonNumberHandling.AllowReadingFromString)]
+                        public long ReturnTimestamp { get; set; }
+                    }
+                }
+
+                /// <summary>
+                /// 获取或设置售后单号。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("aftersale_id")]
+                [System.Text.Json.Serialization.JsonPropertyName("aftersale_id")]
+                public long AftersaleOrderId { get; set; }
+
+                /// <summary>
+                /// 获取或设置商家自定义售后单号。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("out_aftersale_id")]
+                [System.Text.Json.Serialization.JsonPropertyName("out_aftersale_id")]
+                public string OutAftersaleOrderId { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置订单 ID。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("order_id")]
+                [System.Text.Json.Serialization.JsonPropertyName("order_id")]
+                public long OrderId { get; set; }
+
+                /// <summary>
+                /// 获取或设置商家自定义订单号。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("out_order_id")]
+                [System.Text.Json.Serialization.JsonPropertyName("out_order_id")]
+                public string OutOrderId { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置类型。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("type")]
+                [System.Text.Json.Serialization.JsonPropertyName("type")]
+                public int Type { get; set; }
+
+                /// <summary>
+                /// 获取或设置状态。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("status")]
+                [System.Text.Json.Serialization.JsonPropertyName("status")]
+                public int Status { get; set; }
+
+                /// <summary>
+                /// 获取或设置退款金额（单位：分）。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("orderamt")]
+                [System.Text.Json.Serialization.JsonPropertyName("orderamt")]
+                public int RefundAmount { get; set; }
+
+                /// <summary>
+                /// 获取或设置退款原因类型。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("refund_reason_type")]
+                [System.Text.Json.Serialization.JsonPropertyName("refund_reason_type")]
+                public int RefundReasonType { get; set; }
+
+                /// <summary>
+                /// 获取或设置退款原因。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("refund_reason")]
+                [System.Text.Json.Serialization.JsonPropertyName("refund_reason")]
+                public string RefundReason { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置创建时间。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("create_time")]
+                [System.Text.Json.Serialization.JsonPropertyName("create_time")]
+                [System.Text.Json.Serialization.JsonNumberHandling(System.Text.Json.Serialization.JsonNumberHandling.AllowReadingFromString)]
+                public long CreateTimeTimestamp { get; set; }
+
+                /// <summary>
+                /// 获取或设置更新时间。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("update_time")]
+                [System.Text.Json.Serialization.JsonPropertyName("update_time")]
+                [System.Text.Json.Serialization.JsonNumberHandling(System.Text.Json.Serialization.JsonNumberHandling.AllowReadingFromString)]
+                public long UpdateTimeTimestamp { get; set; }
+
+                /// <summary>
+                /// 获取或设置商品信息。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("product_info")]
+                [System.Text.Json.Serialization.JsonPropertyName("product_info")]
+                public Types.Product Product { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置退货信息。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("return_info")]
+                [System.Text.Json.Serialization.JsonPropertyName("return_info")]
+                public Types.Return? Return { get; set; }
             }
         }
 
