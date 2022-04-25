12909dd9696d45c35f1dc2b663d174f78253ac81
MoondanceZ <Qin_Yikai@outlook.com>
2022-04-25 18:38:33 +0800

    feat(wxapi): 更新自定义交易组件获取售后详情模型

--- src/SKIT.FlurlHttpClient.Wechat.Api/Models/Shop/ECAftersale/ShopECAftersaleGetResponse.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/Shop/ECAftersale/ShopECAftersaleGetResponse.cs
@@ -14,18 +14,32 @@
                     public class Product
                     {
                         /// <summary>
+                        /// 获取或设置商品 ID。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("product_id")]
+                        [System.Text.Json.Serialization.JsonPropertyName("product_id")]
+                        public long ProductId { get; set; }
+
+                        /// <summary>
                         /// 获取或设置商家自定义商品 ID。
                         /// </summary>
                         [Newtonsoft.Json.JsonProperty("out_product_id")]
                         [System.Text.Json.Serialization.JsonPropertyName("out_product_id")]
-                        public string? OutProductId { get; set; }
+                        public string OutProductId { get; set; } = default!;
+
+                        /// <summary>
+                        /// 获取或设置 SKU ID。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("sku_id")]
+                        [System.Text.Json.Serialization.JsonPropertyName("sku_id")]
+                        public long SKUId { get; set; }
 
                         /// <summary>
                         /// 获取或设置商家自定义 SKU ID。
                         /// </summary>
                         [Newtonsoft.Json.JsonProperty("out_sku_id")]
                         [System.Text.Json.Serialization.JsonPropertyName("out_sku_id")]
-                        public string? OutSKUId { get; set; }
+                        public string OutSKUId { get; set; } = default!;
 
                         /// <summary>
                         /// 获取或设置参与售后的商品数量。
@@ -65,6 +79,40 @@
                         [System.Text.Json.Serialization.JsonPropertyName("order_return_time")]
                         [System.Text.Json.Serialization.JsonNumberHandling(System.Text.Json.Serialization.JsonNumberHandling.AllowReadingFromString)]
                         public long ReturnTimestamp { get; set; }
+                    }
+
+                    public class Media
+                    {
+                        /// <summary>
+                        /// 获取或设置类型。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("type")]
+                        [System.Text.Json.Serialization.JsonPropertyName("type")]
+                        public int Type { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置 URL。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("url")]
+                        [System.Text.Json.Serialization.JsonPropertyName("url")]
+                        public string Url { get; set; } = string.Empty;
+
+                        /// <summary>
+                        /// 获取或设置 URL。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("thumb_url")]
+                        [System.Text.Json.Serialization.JsonPropertyName("thumb_url")]
+                        public string ThumbnailUrl { get; set; } = string.Empty;
+                    }
+
+                    public class RefundPayDetail
+                    {
+                        /// <summary>
+                        /// 获取或设置退款单号。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("refund_id")]
+                        [System.Text.Json.Serialization.JsonPropertyName("refund_id")]
+                        public string RefundId { get; set; } = default!;
                     }
                 }
 
@@ -148,6 +196,27 @@
                 public long UpdateTimeTimestamp { get; set; }
 
                 /// <summary>
+                /// 获取或设置用户 OpenId。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("openid")]
+                [System.Text.Json.Serialization.JsonPropertyName("openid")]
+                public string OpenId { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置 ReturnId。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("return_id")]
+                [System.Text.Json.Serialization.JsonPropertyName("return_id")]
+                public string ReturnId { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置附件列表。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("media_list")]
+                [System.Text.Json.Serialization.JsonPropertyName("media_list")]
+                public Types.Media[] MediaList { get; set; } = default!;
+
+                /// <summary>
                 /// 获取或设置商品信息。
                 /// </summary>
                 [Newtonsoft.Json.JsonProperty("product_info")]
@@ -160,6 +229,13 @@
                 [Newtonsoft.Json.JsonProperty("return_info")]
                 [System.Text.Json.Serialization.JsonPropertyName("return_info")]
                 public Types.Return? Return { get; set; }
+
+                /// <summary>
+                /// 获取或设置退款信息。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("refund_pay_detail")]
+                [System.Text.Json.Serialization.JsonPropertyName("refund_pay_detail")]
+                public Types.RefundPayDetail? RefundPayDetail { get; set; }
             }
         }
 
