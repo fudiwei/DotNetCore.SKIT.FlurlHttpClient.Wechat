e963eb902e5d2c47b944a2049d3c7fc3383ebe0d
Fu Diwei <fudiwei@sina.com>
2022-05-01 21:21:22 +0800

    feat(wxapi): 新增第三方平台半屏小程序管理相关接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/WxaApi/WxaEmbedded/WxaApiWxaEmbeddedAddEmbeddedResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Api.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /wxaapi/wxaembedded/add_embedded 接口的响应。</para>
+    /// </summary>
+    public class WxaApiWxaEmbeddedAddEmbeddedResponse : WechatApiResponse
+    {
+    }
+}
