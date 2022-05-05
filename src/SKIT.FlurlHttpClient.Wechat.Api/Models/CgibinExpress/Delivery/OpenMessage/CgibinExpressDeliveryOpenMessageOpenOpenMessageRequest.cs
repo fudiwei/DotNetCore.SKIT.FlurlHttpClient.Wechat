0583ad512f245eee914ca420fc5c6ec90db98589
MoondanceZ <Qin_Yikai@outlook.com>
2022-05-05 10:17:58 +0800

    feat(wxapi): 新增云开通物流服务相关接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/CgibinExpress/Delivery/OpenMessage/CgibinExpressDeliveryOpenMessageOpenOpenMessageRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.Wechat.Api.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] cgi-bin/express/delivery/open_msg/open_openmsg 接口的请求。</para>
+    /// </summary>
+    public class CgibinExpressDeliveryOpenMessageOpenOpenMessageRequest : WechatApiRequest, IInferable<CgibinExpressDeliveryOpenMessageOpenOpenMessageRequest, CgibinExpressDeliveryOpenMessageOpenOpenMessageResponse>
+    {
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/CgibinExpress/Delivery/OpenMessage/CgibinExpressDeliveryOpenMessageOpenQueryPluginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Api.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /cgi-bin/express/delivery/open_msg/open_query_plugin 接口的响应。</para>
+    /// </summary>
+    public class CgibinExpressDeliveryOpenMessageOpenQueryPluginResponse : WechatApiResponse
+    {
+    }
+}
