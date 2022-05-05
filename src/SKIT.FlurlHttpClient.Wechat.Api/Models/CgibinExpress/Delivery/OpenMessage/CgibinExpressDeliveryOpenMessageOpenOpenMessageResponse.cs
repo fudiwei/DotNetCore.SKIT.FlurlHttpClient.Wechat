b9e7315626051f21092cf91e8b87a8cb68b54c00
RHQYZ <fudiwei@sina.com>
2022-05-05 15:27:03 +0800

    Create CgibinExpressDeliveryOpenMessageOpenOpenMessageResponse.cs

--- src/SKIT.FlurlHttpClient.Wechat.Api/Models/CgibinExpress/Delivery/OpenMessage/CgibinExpressDeliveryOpenMessageOpenOpenMessageResponse.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/CgibinExpress/Delivery/OpenMessage/CgibinExpressDeliveryOpenMessageOpenOpenMessageResponse.cs
@@ -1,7 +1,7 @@
 ﻿namespace SKIT.FlurlHttpClient.Wechat.Api.Models
 {
     /// <summary>
-    /// <para>表示 [POST] cgi-bin/express/delivery/open_msg/open_openmsg 接口的响应。</para>
+    /// <para>表示 [POST] /cgi-bin/express/delivery/open_msg/open_openmsg 接口的响应。</para>
     /// </summary>
     public class CgibinExpressDeliveryOpenMessageOpenOpenMessageResponse : WechatApiResponse
     {
