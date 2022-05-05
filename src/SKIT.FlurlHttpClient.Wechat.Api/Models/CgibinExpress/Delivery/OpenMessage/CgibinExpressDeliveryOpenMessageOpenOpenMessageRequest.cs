b6b429609c1d339681472e231d0d7a4f067a20c9
RHQYZ <fudiwei@sina.com>
2022-05-05 15:27:37 +0800

    Update CgibinExpressDeliveryOpenMessageOpenOpenMessageRequest.cs

--- src/SKIT.FlurlHttpClient.Wechat.Api/Models/CgibinExpress/Delivery/OpenMessage/CgibinExpressDeliveryOpenMessageOpenOpenMessageRequest.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/CgibinExpress/Delivery/OpenMessage/CgibinExpressDeliveryOpenMessageOpenOpenMessageRequest.cs
@@ -3,7 +3,7 @@
 namespace SKIT.FlurlHttpClient.Wechat.Api.Models
 {
     /// <summary>
-    /// <para>表示 [POST] cgi-bin/express/delivery/open_msg/open_openmsg 接口的请求。</para>
+    /// <para>表示 [POST] /cgi-bin/express/delivery/open_msg/open_openmsg 接口的请求。</para>
     /// </summary>
     public class CgibinExpressDeliveryOpenMessageOpenOpenMessageRequest : WechatApiRequest, IInferable<CgibinExpressDeliveryOpenMessageOpenOpenMessageRequest, CgibinExpressDeliveryOpenMessageOpenOpenMessageResponse>
     {
