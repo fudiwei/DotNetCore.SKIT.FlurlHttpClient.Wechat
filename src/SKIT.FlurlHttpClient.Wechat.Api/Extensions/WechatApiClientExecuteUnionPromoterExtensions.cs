870f5b339b5b613a8c9bb0f913838e7a8fb71d17
Fu Diwei <fudiwei@sina.com>
2022-06-19 22:05:16 +0800

    feat(wxapi): 新增小程序联盟查询商品推广订单明细接口

--- src/SKIT.FlurlHttpClient.Wechat.Api/Extensions/WechatApiClientExecuteUnionPromoterExtensions.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Extensions/WechatApiClientExecuteUnionPromoterExtensions.cs
@@ -390,6 +390,67 @@ namespace SKIT.FlurlHttpClient.Wechat.Api
 
             return await client.SendRequestWithJsonAsync<Models.UnionPromoterOrderSearchResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
         }
+
+        /// <summary>
+        /// <para>异步调用 [GET] /union/promoter/order/search_normal_order 接口。</para>
+        /// <para>REF: https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/business-capabilities/union/custom_component/api/promoter/order-info.html </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.UnionPromoterOrderSearchNormalOrderResponse> ExecuteUnionPromoterOrderSearchNormalOrderAsync(this WechatApiClient client, Models.UnionPromoterOrderSearchNormalOrderRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Get, "union", "promoter", "order", "search_normal_order")
+                .SetQueryParam("access_token", request.AccessToken)
+                .SetQueryParam("offset", request.Offset)
+                .SetQueryParam("limit", request.Limit);
+
+            if (request.Query != null)
+                flurlReq.SetQueryParam("query", request.Query);
+
+            if (request.OrderId != null)
+                flurlReq.SetQueryParam("orderId", request.OrderId.Value);
+
+            if (request.OutOrderId != null)
+                flurlReq.SetQueryParam("outOrderId", request.OutOrderId);
+
+            if (request.OutOrderIdList != null)
+                flurlReq.SetQueryParam("outOrderIdList", string.Join(",", request.OutOrderId));
+
+            if (request.TransactionId != null)
+                flurlReq.SetQueryParam("tradeNo", request.TransactionId);
+
+            if (request.TransactionIdList != null)
+                flurlReq.SetQueryParam("transactionIdList", string.Join(",", request.TransactionIdList));
+
+            if (request.StartPayTimestamp != null)
+                flurlReq.SetQueryParam("startPayTime", request.StartPayTimestamp.Value);
+
+            if (request.EndPayTimestamp != null)
+                flurlReq.SetQueryParam("endPayTime", request.EndPayTimestamp.Value);
+
+            if (request.CommissionStatus != null)
+                flurlReq.SetQueryParam("commissionStatus", request.CommissionStatus);
+
+            if (request.StartStatusUpdateTimestamp != null)
+                flurlReq.SetQueryParam("startStatusUpdateTime", request.StartStatusUpdateTimestamp.Value);
+
+            if (request.EndStatusUpdateTimestamp != null)
+                flurlReq.SetQueryParam("endStatusUpdateTime", request.EndStatusUpdateTimestamp.Value);
+
+            if (request.SortBy != null)
+                flurlReq.SetQueryParam("sortBy", request.SortBy);
+
+            if (request.SortOrder != null)
+                flurlReq.SetQueryParam("sortOrder", request.SortOrder);
+
+            return await client.SendRequestWithJsonAsync<Models.UnionPromoterOrderSearchNormalOrderResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
         #endregion
 
         #region Target
