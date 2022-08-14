5dbc0a77ad090db74319410908eb4df33a8dde55
Fu Diwei <fudiwei@sina.com>
2022-08-14 22:06:20 +0800

    feat(work): 随官方更新获取企业上下游通讯录下的企业信息的接口模型

--- src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinCorpGroup/Corp/CgibinCorpGroupCorpGetChainCorpInfoResponse.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinCorpGroup/Corp/CgibinCorpGroupCorpGetChainCorpInfoResponse.cs
@@ -18,5 +18,26 @@
         [Newtonsoft.Json.JsonProperty("custom_id")]
         [System.Text.Json.Serialization.JsonPropertyName("custom_id")]
         public string? CustomId { get; set; }
+
+        /// <summary>
+        /// 获取或设置企业名称。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("corp_name")]
+        [System.Text.Json.Serialization.JsonPropertyName("corp_name")]
+        public string CorpName { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置企业是否验证。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("corp_verified")]
+        [System.Text.Json.Serialization.JsonPropertyName("corp_verified")]
+        public bool IsCorpVerified { get; set; }
+
+        /// <summary>
+        /// 获取或设置企业是否认证。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("corp_certified")]
+        [System.Text.Json.Serialization.JsonPropertyName("corp_certified")]
+        public bool IsCorpCertified { get; set; }
     }
 }
