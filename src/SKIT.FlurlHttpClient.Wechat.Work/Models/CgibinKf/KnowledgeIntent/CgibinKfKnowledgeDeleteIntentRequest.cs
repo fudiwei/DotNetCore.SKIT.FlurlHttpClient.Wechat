141888e2c3fd03dd6041355104e83c2502f8042f
Fu Diwei <fudiwei@sina.com>
2022-08-15 21:22:56 +0800

    feat(work): 新增微信客服知识库相关接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinKf/KnowledgeIntent/CgibinKfKnowledgeDeleteIntentRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /cgi-bin/kf/knowledge/del_intent 接口的请求。</para>
+    /// </summary>
+    public class CgibinKfKnowledgeDeleteIntentRequest : WechatWorkRequest
+    {
+        /// <summary>
+        /// 获取或设置问答 ID。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("intent_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("intent_id")]
+        public string IntentId { get; set; } = string.Empty;
+    }
+}
