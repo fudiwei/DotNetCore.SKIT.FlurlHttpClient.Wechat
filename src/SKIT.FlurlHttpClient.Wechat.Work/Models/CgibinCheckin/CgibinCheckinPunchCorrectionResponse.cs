fabc25935f1c5204fed47a6e9ab56381cc114193
Fu Diwei <fudiwei@sina.com>
2022-08-15 19:58:39 +0800

    feat(work): 新增为打卡人员补卡相关接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinCheckin/CgibinCheckinPunchCorrectionResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /cgi-bin/checkin/punch_correction 接口的响应。</para>
+    /// </summary>
+    public class CgibinCheckinPunchCorrectionResponse : WechatWorkResponse
+    {
+    }
+}
