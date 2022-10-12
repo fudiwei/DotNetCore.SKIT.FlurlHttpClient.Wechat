dfb8f0e58fcba5f7ebf6fa6c5a06e947f20ea1f1
Fu Diwei <fudiwei@sina.com>
2022-10-12 16:37:24 +0800

    feat(work): 随官方更新日程相关接口模型

--- src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinOA/Schedule/CgibinOAScheduleDeleteRequest.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinOA/Schedule/CgibinOAScheduleDeleteRequest.cs
@@ -11,5 +11,19 @@
         [Newtonsoft.Json.JsonProperty("schedule_id")]
         [System.Text.Json.Serialization.JsonPropertyName("schedule_id")]
         public string ScheduleId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 获取或设置操作模式。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("op_mode")]
+        [System.Text.Json.Serialization.JsonPropertyName("op_mode")]
+        public int? OperateMode { get; set; }
+
+        /// <summary>
+        /// 获取或设置操作起始时间戳。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("op_start_time")]
+        [System.Text.Json.Serialization.JsonPropertyName("op_start_time")]
+        public long? OperateStartTimestamp { get; set; }
     }
 }
