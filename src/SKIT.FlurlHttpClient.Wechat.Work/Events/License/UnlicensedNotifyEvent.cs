8dc22d11b762e5e7e7ad9c28f4d960e2c1a41871
Fu Diwei <fudiwei@sina.com>
2022-10-31 11:58:06 +0800

    feat(work): 新增接口调用许可应用管理相关接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Events/License/UnlicensedNotifyEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Events
+{
+    /// <summary>
+    /// <para>表示 EVENT.unlicensed_notify 事件的数据。</para>
+    /// <para>REF: https://developer.work.weixin.qq.com/document/path/95716 </para>
+    /// </summary>
+    public class UnlicensedNotifyEvent : WechatWorkEvent, WechatWorkEvent.Serialization.IXmlSerializable
+    {
+        /// <summary>
+        /// 获取或设置应用 ID。
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("AgentID")]
+        public int AgentId { get; set; }
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Extensions/WechatWorkClientExecuteCgibinLicenseExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace SKIT.FlurlHttpClient.Wechat.Work
+{
+    public static class WechatWorkClientExecuteCgibinLicenseExtensions
+    {
+        #region App
+        /// <summary>
+        /// <para>异步调用 [POST] /cgi-bin/license/get_app_license_info 接口。</para>
+        /// <para>REF: https://developer.work.weixin.qq.com/document/path/95844 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.CgibinLicenseGetAppLicenseInfoResponse> ExecuteCgibinLicenseGetAppLicenseInfoAsync(this WechatWorkClient client, Models.CgibinLicenseGetAppLicenseInfoRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            if (request.SuiteId == null)
+                request.SuiteId = client.Credentials.SuiteId;
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "cgi-bin", "license", "get_app_license_info")
+                .SetQueryParam("access_token", request.ProviderAccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.CgibinLicenseGetAppLicenseInfoResponse>(flurlReq, cancellationToken: cancellationToken);
+        }
+        #endregion
+    }
+}
