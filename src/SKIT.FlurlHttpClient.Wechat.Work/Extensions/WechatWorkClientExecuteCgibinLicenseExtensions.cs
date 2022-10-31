ca18ed6cde1b02fae7dfce93216d1114a198f82e
Fu Diwei <fudiwei@sina.com>
2022-10-31 12:03:10 +0800

    feat(work): 新增接口调用许可自动激活设置相关接口

--- src/SKIT.FlurlHttpClient.Wechat.Work/Extensions/WechatWorkClientExecuteCgibinLicenseExtensions.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Extensions/WechatWorkClientExecuteCgibinLicenseExtensions.cs
@@ -33,5 +33,47 @@ namespace SKIT.FlurlHttpClient.Wechat.Work
             return await client.SendRequestWithJsonAsync<Models.CgibinLicenseGetAppLicenseInfoResponse>(flurlReq, cancellationToken: cancellationToken);
         }
         #endregion
+
+        #region App
+        /// <summary>
+        /// <para>异步调用 [POST] /cgi-bin/license/set_auto_active_status 接口。</para>
+        /// <para>REF: https://developer.work.weixin.qq.com/document/path/95873 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.CgibinLicenseSetAutoActiveStatusResponse> ExecuteCgibinLicenseSetAutoActiveStatusAsync(this WechatWorkClient client, Models.CgibinLicenseSetAutoActiveStatusRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "cgi-bin", "license", "set_auto_active_status")
+                .SetQueryParam("access_token", request.ProviderAccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.CgibinLicenseSetAutoActiveStatusResponse>(flurlReq, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// <para>异步调用 [POST] /cgi-bin/license/get_auto_active_status 接口。</para>
+        /// <para>REF: https://developer.work.weixin.qq.com/document/path/95874 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.CgibinLicenseGetAutoActiveStatusResponse> ExecuteCgibinLicenseGetAutoActiveStatusAsync(this WechatWorkClient client, Models.CgibinLicenseGetAutoActiveStatusRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "cgi-bin", "license", "get_auto_active_status")
+                .SetQueryParam("access_token", request.ProviderAccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.CgibinLicenseGetAutoActiveStatusResponse>(flurlReq, cancellationToken: cancellationToken);
+        }
+        #endregion
     }
 }
---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinLicense/AutoActive/CgibinLicenseSetAutoActiveStatusResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /cgi-bin/license/set_auto_active_status 接口的响应。</para>
+    /// </summary>
+    public class CgibinLicenseSetAutoActiveStatusResponse : WechatWorkResponse
+    {
+    }
+}
