4e3317dadabc522174cf92f0b46e3dbe71bf992b
吴俊毅 <dydhyh@foxmail.com>
2022-11-17 17:20:07 +0800

    feat(openai): AIBotRequest、SignRequest添加Json注解

--- src/SKIT.FlurlHttpClient.Wechat.OpenAI/Models/Platform/Query/AIBotRequest.cs
+++ src/SKIT.FlurlHttpClient.Wechat.OpenAI/Models/Platform/Query/AIBotRequest.cs
@@ -13,18 +13,21 @@ namespace SKIT.FlurlHttpClient.Wechat.OpenAI.Models.Platform
         /// 获取或设置请求签名。
         /// </summary>
         [XmlElement("signature")]
+        [JsonPropertyName("signature")]
         public string Signature { get; set; } = string.Empty;
 
         /// <summary>
         /// 获取或设置询问语句。
         /// </summary>
         [XmlElement("query")]
+        [JsonPropertyName("query")]
         public string QueryString { get; set; } = string.Empty;
 
         /// <summary>
         /// 获取或设置环境。
         /// </summary>
         [XmlElement("env", IsNullable = true)]
+        [JsonPropertyName("env")]
         public string? Environment { get; set; }
     }
 }
--- src/SKIT.FlurlHttpClient.Wechat.OpenAI/Models/Platform/Query/SignRequest.cs
+++ src/SKIT.FlurlHttpClient.Wechat.OpenAI/Models/Platform/Query/SignRequest.cs
@@ -13,18 +13,21 @@ namespace SKIT.FlurlHttpClient.Wechat.OpenAI.Models.Platform
         /// 获取或设置用户 ID。
         /// </summary>
         [XmlElement("userid")]
+        [JsonPropertyName("userid")]
         public string UserId { get; set; } = string.Empty;
 
         /// <summary>
         /// 获取或设置用户昵称。
         /// </summary>
         [XmlElement("username")]
+        [JsonPropertyName("username")]
         public string Nickname { get; set; } = string.Empty;
 
         /// <summary>
         /// 获取或设置用户头像 URL。
         /// </summary>
         [XmlElement("avatar", IsNullable = true)]
+        [JsonPropertyName("avatar")]
         public string? AvatarUrl { get; set; }
     }
 }
