2dad95ba62464b67bf4513759c5a9a029a771ecb
RHQYZ <fudiwei@sina.com>
2022-11-18 17:32:58 +0800

    Update SignRequest.cs

--- src/SKIT.FlurlHttpClient.Wechat.OpenAI/Models/Platform/Query/SignRequest.cs
+++ src/SKIT.FlurlHttpClient.Wechat.OpenAI/Models/Platform/Query/SignRequest.cs
@@ -1,33 +1,29 @@
-﻿using System.Text.Json.Serialization;
-using System.Xml.Serialization;
-
 namespace SKIT.FlurlHttpClient.Wechat.OpenAI.Models.Platform
 {
     /// <summary>
     /// <para>表示 [POST] /sign/{TOKEN} 接口的请求。</para>
     /// </summary>
-    [XmlRoot("xml")]
     public class SignRequest : WechatOpenAIPlatformRequest
     {
         /// <summary>
         /// 获取或设置用户 ID。
         /// </summary>
-        [XmlElement("userid")]
-        [JsonPropertyName("userid")]
+        [Newtonsoft.Json.JsonProperty("userid")]
+        [System.Text.Json.Serialization.JsonPropertyName("userid")]
         public string UserId { get; set; } = string.Empty;
 
         /// <summary>
         /// 获取或设置用户昵称。
         /// </summary>
-        [XmlElement("username")]
-        [JsonPropertyName("username")]
-        public string Nickname { get; set; } = string.Empty;
+        [Newtonsoft.Json.JsonProperty("username")]
+        [System.Text.Json.Serialization.JsonPropertyName("username")]
+        public string? Nickname { get; set; }
 
         /// <summary>
         /// 获取或设置用户头像 URL。
         /// </summary>
-        [XmlElement("avatar", IsNullable = true)]
-        [JsonPropertyName("avatar")]
+        [Newtonsoft.Json.JsonProperty("avatar")]
+        [System.Text.Json.Serialization.JsonPropertyName("avatar")]
         public string? AvatarUrl { get; set; }
     }
 }
