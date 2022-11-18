71d494ae14f2bdb282677c9085e323cf65ea8a29
dydhyh <dydhyh@163.com>
2022-11-18 18:59:26 +0800

    feat(openai): 随官方更新智能对话接口相关接口模型

--- src/SKIT.FlurlHttpClient.Wechat.OpenAI/Models/Platform/Query/AIBotRequest.cs
+++ src/SKIT.FlurlHttpClient.Wechat.OpenAI/Models/Platform/Query/AIBotRequest.cs
@@ -1,29 +1,45 @@
-﻿using System.Xml.Serialization;
+using System.Collections.Generic;
 
 namespace SKIT.FlurlHttpClient.Wechat.OpenAI.Models.Platform
 {
     /// <summary>
     /// <para>表示 [POST] /aibot/{TOKEN} 接口的请求。</para>
     /// </summary>
-    [XmlRoot("xml")]
-    public class AIBotRequest : WechatOpenAIPlatformRequest, WechatOpenAIPlatformRequest.Serialization.IEncryptedXmlable
+    public class AIBotRequest : WechatOpenAIPlatformRequest
     {
         /// <summary>
         /// 获取或设置请求签名。
         /// </summary>
-        [XmlElement("signature")]
+        [Newtonsoft.Json.JsonProperty("signature")]
+        [System.Text.Json.Serialization.JsonPropertyName("signature")]
         public string Signature { get; set; } = string.Empty;
 
         /// <summary>
         /// 获取或设置询问语句。
         /// </summary>
-        [XmlElement("query")]
+        [Newtonsoft.Json.JsonProperty("query")]
+        [System.Text.Json.Serialization.JsonPropertyName("query")]
         public string QueryString { get; set; } = string.Empty;
 
         /// <summary>
         /// 获取或设置环境。
         /// </summary>
-        [XmlElement("env", IsNullable = true)]
+        [Newtonsoft.Json.JsonProperty("env")]
+        [System.Text.Json.Serialization.JsonPropertyName("env")]
         public string? Environment { get; set; }
+
+        /// <summary>
+        /// 获取或设置第一优先级的限定技能命中范围列表。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("first_priority_skills")]
+        [System.Text.Json.Serialization.JsonPropertyName("first_priority_skills")]
+        public IList<string>? FirstPrioritySkillList { get; set; }
+
+        /// <summary>
+        /// 获取或设置第二优先级的限定技能命中范围列表。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("second_priority_skills")]
+        [System.Text.Json.Serialization.JsonPropertyName("second_priority_skills")]
+        public IList<string>? SecondPrioritySkillList { get; set; }
     }
 }
