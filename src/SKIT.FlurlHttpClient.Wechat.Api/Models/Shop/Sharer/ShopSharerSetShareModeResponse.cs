488324b2834aac2bdd749ff11ae4404f6d6f0357
fudiwei <fudiwei@sina.com>
2022-12-01 22:42:32 +0800

    feat(wxapi): 新增自定义交易组件设置小程序分享模式接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/Shop/Sharer/ShopSharerSetShareModeResponse.cs
@@ -0,0 +1,9 @@
+namespace SKIT.FlurlHttpClient.Wechat.Api.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /shop/sharer/set_share_mode 接口的响应。</para>
+    /// </summary>
+    public class ShopSharerSetShareModeResponse : WechatApiResponse
+    {
+    }
+}
