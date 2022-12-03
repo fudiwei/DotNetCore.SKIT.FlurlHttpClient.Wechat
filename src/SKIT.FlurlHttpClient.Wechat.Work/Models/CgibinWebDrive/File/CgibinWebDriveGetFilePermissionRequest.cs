d024f45c004ed551b34b02a59e393b8e6c7fc3db
fudiwei <fudiwei@sina.com>
2022-12-03 18:00:38 +0800

    feat(work): 新增获取微盘文件权限信息接口

---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinWebDrive/File/CgibinWebDriveGetFilePermissionRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /cgi-bin/wedrive/get_file_permission 接口的请求。</para>
+    /// </summary>
+    public class CgibinWebDriveGetFilePermissionRequest : WechatWorkRequest
+    {
+        /// <summary>
+        /// 获取或设置文件 ID。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("fileid")]
+        [System.Text.Json.Serialization.JsonPropertyName("fileid")]
+        public string FileId { get; set; } = string.Empty;
+    }
+}
