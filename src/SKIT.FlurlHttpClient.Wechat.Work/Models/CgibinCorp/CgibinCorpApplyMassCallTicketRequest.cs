b58fd63de9467032d791cb40ef5f49c00623d803
fudiwei <fudiwei@sina.com>
2022-12-04 02:53:45 +0800

    feat(work): 新增获取接口大批量调用凭据

---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinCorp/CgibinCorpApplyMassCallTicketRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /cgi-bin/corp/apply_mass_call_ticket 接口的请求。</para>
+    /// </summary>
+    public class CgibinCorpApplyMassCallTicketRequest : WechatWorkRequest
+    {
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinCorp/CgibinCorpApplyMassCallTicketResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /cgi-bin/corp/apply_mass_call_ticket 接口的响应。</para>
+    /// </summary>
+    public class CgibinCorpApplyMassCallTicketResponse : WechatWorkResponse
+    {
+        /// <summary>
+        /// 获取或设置大批量调用凭据。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("mass_call_ticket")]
+        [System.Text.Json.Serialization.JsonPropertyName("mass_call_ticket")]
+        public string MassCallTicket { get; set; } = default!;
+    }
+}
