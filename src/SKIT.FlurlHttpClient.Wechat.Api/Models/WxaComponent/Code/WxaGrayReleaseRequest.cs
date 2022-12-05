cd755dec550de754dc8b9605608ef94e47b8be82
Qin Yikai <QYK_zZ@hotmail.com>
2022-12-05 21:00:02 +0800

    feat(wxapi): 随官方更新第三方平台小程序分阶段发布接口模型

--- src/SKIT.FlurlHttpClient.Wechat.Api/Models/WxaComponent/Code/WxaGrayReleaseRequest.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/WxaComponent/Code/WxaGrayReleaseRequest.cs
@@ -1,4 +1,4 @@
-﻿namespace SKIT.FlurlHttpClient.Wechat.Api.Models
+namespace SKIT.FlurlHttpClient.Wechat.Api.Models
 {
     /// <summary>
     /// <para>表示 [POST] /wxa/grayrelease 接口的请求。</para>
@@ -11,5 +11,19 @@
         [Newtonsoft.Json.JsonProperty("gray_percentage")]
         [System.Text.Json.Serialization.JsonPropertyName("gray_percentage")]
         public int GrayPercentage { get; set; }
+
+        /// <summary>
+        /// 获取或设置是否支持按项目成员灰度。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("support_debuger_first")]
+        [System.Text.Json.Serialization.JsonPropertyName("support_debuger_first")]
+        public bool? IsSupportDebuggerFirst { get; set; }
+
+        /// <summary>
+        /// 获取或设置是否支持按体验成员灰度。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("support_experiencer_first")]
+        [System.Text.Json.Serialization.JsonPropertyName("support_experiencer_first")]
+        public bool? IsSupportExperiencerFirst { get; set; }
     }
 }
