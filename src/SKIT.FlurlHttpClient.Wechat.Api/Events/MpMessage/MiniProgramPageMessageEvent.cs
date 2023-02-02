5608be6c515366f9b11e9166fc55acb10139da2a
Fu Diwei <fudiwei@sina.com>
2023-02-02 22:23:18 +0800

    feat(wxapi): 新增若干回调通知事件模型

---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Events/MpMessage/MiniProgramPageMessageEvent.cs
@@ -0,0 +1,57 @@
+namespace SKIT.FlurlHttpClient.Wechat.Api.Events
+{
+    /// <summary>
+    /// <para>表示 MINIPROGRAMPAGE 事件的数据。</para>
+    /// <para>REF: https://developers.weixin.qq.com/doc/offiaccount/Message_Management/Receiving_standard_messages.html#%E6%96%87%E6%9C%AC%E6%B6%88%E6%81%AF </para>
+    /// </summary>
+    public class MiniProgramPageMessageEvent : WechatApiEvent, WechatApiEvent.Serialization.IJsonSerializable, WechatApiEvent.Serialization.IXmlSerializable
+    {
+        /// <summary>
+        /// 获取或设置消息 ID。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("MsgId")]
+        [System.Text.Json.Serialization.JsonPropertyName("MsgId")]
+        [System.Xml.Serialization.XmlElement("MsgId")]
+        public long MessageId { get; set; }
+
+        /// <summary>
+        /// 获取或设置小程序 AppId。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("AppId")]
+        [System.Text.Json.Serialization.JsonPropertyName("AppId")]
+        [System.Xml.Serialization.XmlElement("AppId")]
+        public string MiniProgramAppId { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置小程序标题。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("Title")]
+        [System.Text.Json.Serialization.JsonPropertyName("Title")]
+        [System.Xml.Serialization.XmlElement("Title")]
+        public string MiniProgramTitle { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置小程序页面路径。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("PagePath")]
+        [System.Text.Json.Serialization.JsonPropertyName("PagePath")]
+        [System.Xml.Serialization.XmlElement("PagePath")]
+        public string MiniProgramPagePath { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置小程序缩略图 URL。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("ThumbUrl")]
+        [System.Text.Json.Serialization.JsonPropertyName("ThumbUrl")]
+        [System.Xml.Serialization.XmlElement("ThumbUrl")]
+        public string MiniProgramThumbnailUrl { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置小程序缩略图 MediaId。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("ThumbMediaId")]
+        [System.Text.Json.Serialization.JsonPropertyName("ThumbMediaId")]
+        [System.Xml.Serialization.XmlElement("ThumbMediaId", IsNullable = true)]
+        public string? MiniProgramThumbnailMediaId { get; set; }
+    }
+}
