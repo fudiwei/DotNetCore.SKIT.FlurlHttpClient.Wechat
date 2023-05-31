52fcc1e1d2b0384339b2d6cd181e4cdecaede2f1
RHQYZ <fudiwei@sina.com>
2023-05-31 16:49:05 +0800

    Update CgibinStableTokenResponse.cs

--- src/SKIT.FlurlHttpClient.Wechat.Api/Models/Cgibin/CgibinStableTokenResponse.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/Cgibin/CgibinStableTokenResponse.cs
@@ -3,20 +3,7 @@ namespace SKIT.FlurlHttpClient.Wechat.Api.Models
     /// <summary>
     /// <para>表示 [POST] /cgi-bin/stable_token 接口的响应。</para>
     /// </summary>
-    public class CgibinStableTokenResponse : WechatApiResponse
+    public class CgibinStableTokenResponse : CgibinTokenResponse
     {
-        /// <summary>
-        /// 获取或设置凭证。
-        /// </summary>
-        [Newtonsoft.Json.JsonProperty("access_token")]
-        [System.Text.Json.Serialization.JsonPropertyName("access_token")]
-        public string AccessToken { get; set; } = default!;
-
-        /// <summary>
-        /// 获取或设置凭证有效期（单位：秒）。
-        /// </summary>
-        [Newtonsoft.Json.JsonProperty("expires_in")]
-        [System.Text.Json.Serialization.JsonPropertyName("expires_in")]
-        public int ExpiresIn { get; set; }
     }
 }
