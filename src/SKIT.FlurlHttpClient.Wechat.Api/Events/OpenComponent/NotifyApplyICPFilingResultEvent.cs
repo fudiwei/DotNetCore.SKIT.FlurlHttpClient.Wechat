d81313a46f34db1afeb5442bd4bb589b68d23ead
xiedi <763171683@qq.com>
2023-09-09 08:30:09 +0800

    微信小程序ICP备案审核通知事件

---
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Events/OpenComponent/NotifyApplyICPFilingResultEvent.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKIT.FlurlHttpClient.Wechat.Api.Events.OpenComponent
+{
+    /// <summary>
+    /// <para>表示 INFO.notify_apply_icpfiling_result 事件的数据。</para>
+    /// <para>REF: https://developers.weixin.qq.com/doc/oplatform/openApi/OpenApiDoc/miniprogram-management/record/applyIcpFiling.html#%E4%BA%8B%E4%BB%B6%E6%8E%A8%E9%80%81 </para>
+    /// </summary>
+    public class NotifyApplyICPFilingResultEvent : WechatApiEvent, WechatApiEvent.Serialization.IXmlSerializable
+    {
+        /// <summary>
+        /// 获取或设置授权方 AppId。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("authorizer_appid")]
+        [System.Text.Json.Serialization.JsonPropertyName("authorizer_appid")]
+        [System.Xml.Serialization.XmlElement("authorizer_appid")]
+        public string AuthorizerAppId { get; set; } = default!;
+
+        /// <summary>
+        /// 备案状态
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("beian_status")]
+        [System.Text.Json.Serialization.JsonPropertyName("beian_status")]
+        [System.Xml.Serialization.XmlElement("beian_status")]
+        public int BeianStatus { get; set; }
+    }
+}
