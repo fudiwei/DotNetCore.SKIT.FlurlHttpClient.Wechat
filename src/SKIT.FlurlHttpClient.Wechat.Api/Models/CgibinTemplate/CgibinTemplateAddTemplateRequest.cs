fa97001206614b1639128d4f4baa265820e77aa5
MoondanceZ <qin_yikai@outlook.com>
2023-09-14 13:14:37 +0000

    feat(wxapi): 随官方更新获得模板消息 ID 接口模型

--- src/SKIT.FlurlHttpClient.Wechat.Api/Models/CgibinTemplate/CgibinTemplateAddTemplateRequest.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Api/Models/CgibinTemplate/CgibinTemplateAddTemplateRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace SKIT.FlurlHttpClient.Wechat.Api.Models
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.Wechat.Api.Models
 {
     /// <summary>
     /// <para>表示 [POST] /cgi-bin/template/api_add_template 接口的请求。</para>
@@ -11,5 +13,12 @@
         [Newtonsoft.Json.JsonProperty("template_id_short")]
         [System.Text.Json.Serialization.JsonPropertyName("template_id_short")]
         public string ShortTemplateId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 获取或设置模板库中模板的关键词。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("keyword_name_list")]
+        [System.Text.Json.Serialization.JsonPropertyName("keyword_name_list")]
+        public IList<string> KeywordNameList { get; set; } = new List<string>();
     }
 }
