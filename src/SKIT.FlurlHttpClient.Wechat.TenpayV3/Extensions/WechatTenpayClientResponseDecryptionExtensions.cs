4ea2a71b2ca1a9a9dbd776858288af86b19ef3b8
Fu Diwei <fudiwei@sina.com>
2024-02-04 11:39:32 +0800

    refactor(tenpayv3): 优化反射性能

--- src/SKIT.FlurlHttpClient.Wechat.TenpayV3/Extensions/WechatTenpayClientResponseDecryptionExtensions.cs
+++ src/SKIT.FlurlHttpClient.Wechat.TenpayV3/Extensions/WechatTenpayClientResponseDecryptionExtensions.cs
@@ -5,11 +5,22 @@ using System.Text;
 
 namespace SKIT.FlurlHttpClient.Wechat.TenpayV3
 {
+    using SKIT.FlurlHttpClient.Wechat.TenpayV3.Models;
+    using SKIT.FlurlHttpClient.Wechat.TenpayV3.Utilities;
+
     public static class WechatTenpayClientResponseDecryptionExtensions
     {
-        private static TResponse InnerDecryptResponseSensitiveProperty<TResponse>(WechatTenpayClient client, TResponse response)
-            where TResponse : Models.QueryCertificatesResponse
+        /// <summary>
+        /// <para>解密响应中返回的敏感数据。该方法会改变传入的响应模型对象。</para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public static QueryCertificatesResponse DecryptResponseSensitiveProperty(this WechatTenpayClient client, QueryCertificatesResponse response)
         {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (response is null) throw new ArgumentNullException(nameof(response));
+
             if (response.CertificateList is null)
                 return response;
 
@@ -25,7 +36,7 @@ namespace SKIT.FlurlHttpClient.Wechat.TenpayV3
                             if (string.IsNullOrEmpty(client.Credentials.MerchantCertificatePrivateKey))
                                 throw new WechatTenpayException("Failed to decrypt response, because the merchant private key is not set.");
 
-                            certificate.EncryptCertificate.CipherText = Utilities.AESUtility.DecryptWithGCM(
+                            certificate.EncryptCertificate.CipherText = AESUtility.DecryptWithGCM(
                                 key: client.Credentials.MerchantV3Secret,
                                 nonce: certificate.EncryptCertificate.Nonce,
                                 aad: certificate.EncryptCertificate.AssociatedData,
@@ -41,11 +52,11 @@ namespace SKIT.FlurlHttpClient.Wechat.TenpayV3
 
                             // REF: https://pay.weixin.qq.com/docs/merchant/development/shangmi/guide.html
                             // 由于 SM4 密钥长度的限制，密钥由 APIv3 密钥通过国密 SM3 Hash 计算生成。SM4 密钥取其摘要（256bit）的前 128bit。
-                            byte[] secretBytes = Utilities.SM3Utility.Hash(Encoding.UTF8.GetBytes(client.Credentials.MerchantV3Secret));
+                            byte[] secretBytes = SM3Utility.Hash(Encoding.UTF8.GetBytes(client.Credentials.MerchantV3Secret));
                             byte[] keyBytes = new byte[16];
                             Array.Copy(secretBytes, keyBytes, keyBytes.Length);
 
-                            byte[] plainBytes = Utilities.SM4Utility.DecryptWithGCM(
+                            byte[] plainBytes = SM4Utility.DecryptWithGCM(
                                 keyBytes: keyBytes,
                                 nonceBytes: Encoding.UTF8.GetBytes(certificate.EncryptCertificate.Nonce),
                                 aadBytes: Encoding.UTF8.GetBytes(certificate.EncryptCertificate.AssociatedData ?? string.Empty),
@@ -62,85 +73,6 @@ namespace SKIT.FlurlHttpClient.Wechat.TenpayV3
                 }
             }
 
-            return response;
-        }
-
-        private static TResponse InnerDecryptResponseSensitivePropertyByRSA<TResponse>(WechatTenpayClient client, TResponse response)
-            where TResponse : WechatTenpayResponse
-        {
-            Utilities.ReflectionHelper.ReplacePropertyStringValue(ref response, (target, currentProp, oldValue) =>
-            {
-                WechatTenpaySensitivePropertyAttribute? attribute = currentProp
-                    .GetCustomAttributes<WechatTenpaySensitivePropertyAttribute>()
-                    .FirstOrDefault(attr => Constants.SignSchemes.WECHATPAY2_RSA_2048_WITH_SHA256.Equals(attr.Scheme));
-                if (attribute is null)
-                    return (false, oldValue);
-
-                string newValue;
-                switch (attribute.Algorithm)
-                {
-                    case Constants.EncryptionAlgorithms.RSA_2048_ECB_PKCS8_OAEP_WITH_SHA1_AND_MGF1:
-                        {
-                            newValue = Utilities.RSAUtility.DecryptWithECB(
-                                privateKey: client.Credentials.MerchantCertificatePrivateKey,
-                                cipherText: oldValue
-                            );
-                        }
-                        break;
-
-                    case Constants.EncryptionAlgorithms.RSA_2048_ECB_PKCS1:
-                        {
-                            newValue = Utilities.RSAUtility.DecryptWithECB(
-                                privateKey: client.Credentials.MerchantCertificatePrivateKey,
-                                cipherText: oldValue,
-                                paddingMode: "PKCS1PADDING"
-                            );
-                        }
-                        break;
-
-                    default:
-                        {
-                            throw new WechatTenpayException($"Failed to decrypt response. Unsupported encryption algorithm: \"{attribute.Algorithm}\".");
-                        }
-                }
-
-                return (true, newValue);
-            });
-            return response;
-        }
-
-        private static TResponse InnerDecryptResponseSensitivePropertyBySM<TResponse>(WechatTenpayClient client, TResponse response)
-            where TResponse : WechatTenpayResponse
-        {
-            Utilities.ReflectionHelper.ReplacePropertyStringValue(ref response, (target, currentProp, oldValue) =>
-            {
-                WechatTenpaySensitivePropertyAttribute? attribute = currentProp
-                    .GetCustomAttributes<WechatTenpaySensitivePropertyAttribute>()
-                    .FirstOrDefault(attr => Constants.SignSchemes.WECHATPAY2_SM2_WITH_SM3.Equals(attr.Scheme));
-                if (attribute is null)
-                    return (false, oldValue);
-
-                string newValue;
-                switch (attribute.Algorithm)
-                {
-                    case Constants.EncryptionAlgorithms.SM2_C1C3C2_ASN1:
-                        {
-                            newValue = Utilities.SM2Utility.Decrypt(
-                                privateKey: client.Credentials.MerchantCertificatePrivateKey,
-                                cipherText: oldValue,
-                                asn1Encoding: true
-                            );
-                        }
-                        break;
-
-                    default:
-                        {
-                            throw new WechatTenpayException($"Failed to decrypt response. Unsupported encryption algorithm: \"{attribute.Algorithm}\".");
-                        }
-                }
-
-                return (true, newValue);
-            });
             return response;
         }
 
@@ -162,24 +94,69 @@ namespace SKIT.FlurlHttpClient.Wechat.TenpayV3
             try
             {
                 // [GET] /certificates 接口的响应模型需特殊处理
-                if (response is Models.QueryCertificatesResponse queryCertificatesResponse)
+                if (response is QueryCertificatesResponse queryCertificatesResponse)
                 {
-                    return (InnerDecryptResponseSensitiveProperty(client, queryCertificatesResponse) as TResponse)!;
+                    return (DecryptResponseSensitiveProperty(client, queryCertificatesResponse) as TResponse)!;
                 }
 
                 // 遍历并解密被标记为敏感数据的字段
                 bool requireDecrypt = Attribute.IsDefined(response.GetType(), typeof(WechatTenpaySensitiveAttribute));
-                if (requireDecrypt)
-                {
-                    switch (client.Credentials.SignScheme)
-                    {
-                        case Constants.SignSchemes.WECHATPAY2_RSA_2048_WITH_SHA256:
-                            return InnerDecryptResponseSensitivePropertyByRSA(client, response);
+                if (!requireDecrypt)
+                    return response;
 
-                        case Constants.SignSchemes.WECHATPAY2_SM2_WITH_SM3:
-                            return InnerDecryptResponseSensitivePropertyBySM(client, response);
+                string signScheme = client.Credentials.SignScheme;
+
+                ReflectionHelper.ReplaceObjectStringProperties(response, (_, currentProp, oldValue) =>
+                {
+                    if (currentProp is null || !currentProp.IsDefined(typeof(WechatTenpaySensitivePropertyAttribute)))
+                        return (false, oldValue);
+
+                    WechatTenpaySensitivePropertyAttribute? attribute = currentProp
+                        .GetCustomAttributes<WechatTenpaySensitivePropertyAttribute>()
+                        .FirstOrDefault(attr => attr.Scheme == signScheme);
+                    if (attribute is null)
+                        return (false, oldValue);
+
+                    string newValue;
+                    switch (attribute.Algorithm)
+                    {
+                        case Constants.EncryptionAlgorithms.RSA_2048_ECB_PKCS8_OAEP_WITH_SHA1_AND_MGF1:
+                            {
+                                newValue = RSAUtility.DecryptWithECB(
+                                    privateKey: client.Credentials.MerchantCertificatePrivateKey,
+                                    cipherText: oldValue
+                                );
+                            }
+                            break;
+
+                        case Constants.EncryptionAlgorithms.RSA_2048_ECB_PKCS1:
+                            {
+                                newValue = RSAUtility.DecryptWithECB(
+                                    privateKey: client.Credentials.MerchantCertificatePrivateKey,
+                                    cipherText: oldValue,
+                                    paddingMode: "PKCS1PADDING"
+                                );
+                            }
+                            break;
+
+                        case Constants.EncryptionAlgorithms.SM2_C1C3C2_ASN1:
+                            {
+                                newValue = SM2Utility.Decrypt(
+                                    privateKey: client.Credentials.MerchantCertificatePrivateKey,
+                                    cipherText: oldValue,
+                                    asn1Encoding: true
+                                );
+                            }
+                            break;
+
+                        default:
+                            {
+                                throw new WechatTenpayException($"Failed to decrypt response. Unsupported encryption algorithm: \"{attribute.Algorithm}\".");
+                            }
                     }
-                }
+
+                    return (true, newValue);
+                });
 
                 return response;
             }
