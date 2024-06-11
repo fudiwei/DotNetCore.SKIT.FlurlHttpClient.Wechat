d2ecffba9b101b94716dfdf2b02b0e11a7b6c61c
Fu Diwei <fudiwei@sina.com>
2024-06-11 23:41:20 +0800

    feat(tenpayv3): 随官方更新合单查询相关接口模型

--- src/SKIT.FlurlHttpClient.Wechat.TenpayV3/Models/CombineTransactions/GetCombineTransactionByCombineOutTradeNumberResponse.cs
+++ src/SKIT.FlurlHttpClient.Wechat.TenpayV3/Models/CombineTransactions/GetCombineTransactionByCombineOutTradeNumberResponse.cs
@@ -42,6 +42,13 @@ namespace SKIT.FlurlHttpClient.Wechat.TenpayV3.Models
                         [Newtonsoft.Json.JsonProperty("payer_currency")]
                         [System.Text.Json.Serialization.JsonPropertyName("payer_currency")]
                         public string PayerCurrency { get; set; } = default!;
+
+                        /// <summary>
+                        /// 获取或设置结算汇率（格式：外币兑换人民币的比例乘以 10 的 8 次方）。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("settlement_rate")]
+                        [System.Text.Json.Serialization.JsonPropertyName("settlement_rate")]
+                        public long? SettlementRate { get; set; }
                     }
                 }
 
