8661caaaa910db333e33cf92f9f10752c82f493c
Fu Diwei <fudiwei@sina.com>
2025-10-13 20:41:36 +0800

    feat(work): 随官方更新企业互联获取下级或下游企业 AccessToken 接口模型

--- src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinCorpGroup/Corp/CgibinCorpGroupCorpGetTokenRequest.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinCorpGroup/Corp/CgibinCorpGroupCorpGetTokenRequest.cs
@@ -1,10 +1,17 @@
-﻿namespace SKIT.FlurlHttpClient.Wechat.Work.Models
+namespace SKIT.FlurlHttpClient.Wechat.Work.Models
 {
     /// <summary>
     /// <para>表示 [POST] /cgi-bin/corpgroup/corp/gettoken 接口的请求。</para>
     /// </summary>
     public class CgibinCorpGroupCorpGetTokenRequest : WechatWorkRequest
     {
+        /// <summary>
+        /// 获取或设置业务类型。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("business_type")]
+        [System.Text.Json.Serialization.JsonPropertyName("business_type")]
+        public int? BusinessType { get; set; }
+
         /// <summary>
         /// 获取或设置下级企业 CorpId。
         /// </summary>
