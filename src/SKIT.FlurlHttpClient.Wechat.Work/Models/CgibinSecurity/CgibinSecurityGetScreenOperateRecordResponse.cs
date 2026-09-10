f488012c11d8c0675405ac6a24c00b3218e270db
Fu Diwei <fudiwei@sina.com>
2026-09-11 00:16:58 +0800

    feat(work): 随官方更新安全管理查询截屏操作记录接口模型

--- src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinSecurity/CgibinSecurityGetScreenOperateRecordResponse.cs
+++ src/SKIT.FlurlHttpClient.Wechat.Work/Models/CgibinSecurity/CgibinSecurityGetScreenOperateRecordResponse.cs
@@ -9,6 +9,122 @@ namespace SKIT.FlurlHttpClient.Wechat.Work.Models
         {
             public class Record
             {
+                public static class Types
+                {
+                    public class DeviceInfo
+                    {
+                        public static class Types
+                        {
+                            public class WindowsDevice
+                            {
+                                /// <summary>
+                                /// 获取或设置 MAC 地址列表。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("mac_addr")]
+                                [System.Text.Json.Serialization.JsonPropertyName("mac_addr")]
+                                public string[]? MacAddressList { get; set; }
+
+                                /// <summary>
+                                /// 获取或设置主板 UUID。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("motherboard_uuid")]
+                                [System.Text.Json.Serialization.JsonPropertyName("motherboard_uuid")]
+                                public string? MotherboardUUID { get; set; }
+
+                                /// <summary>
+                                /// 获取或设置硬盘 UUID 列表。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("harddisk_uuid")]
+                                [System.Text.Json.Serialization.JsonPropertyName("harddisk_uuid")]
+                                public string[]? HardDiskUUIDList { get; set; }
+
+                                /// <summary>
+                                /// 获取或设置域名。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("domain")]
+                                [System.Text.Json.Serialization.JsonPropertyName("domain")]
+                                public string? Domain { get; set; }
+
+                                /// <summary>
+                                /// 获取或设置计算机名。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("pc_name")]
+                                [System.Text.Json.Serialization.JsonPropertyName("pc_name")]
+                                public string? PCName { get; set; }
+                            }
+
+                            public class MacDevice
+                            {
+                                /// <summary>
+                                /// 获取或设置 MAC 地址列表。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("mac_addr")]
+                                [System.Text.Json.Serialization.JsonPropertyName("mac_addr")]
+                                public string[]? MacAddressList { get; set; }
+
+                                /// <summary>
+                                /// 获取或设置序列号。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("seq_no")]
+                                [System.Text.Json.Serialization.JsonPropertyName("seq_no")]
+                                public string? SequenceNumber { get; set; }
+                            }
+
+                            public class MobileDevice
+                            {
+                                /// <summary>
+                                /// 获取或设置设备类型。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("type")]
+                                [System.Text.Json.Serialization.JsonPropertyName("type")]
+                                public int Type { get; set; }
+                            }
+                        }
+
+                        /// <summary>
+                        /// 获取或设置平台类型。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("platform")]
+                        [System.Text.Json.Serialization.JsonPropertyName("platform")]
+                        public string? Platform { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置设备唯一标识。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("device_code")]
+                        [System.Text.Json.Serialization.JsonPropertyName("device_code")]
+                        public string? DeviceCode { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置设备型号。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("device_model")]
+                        [System.Text.Json.Serialization.JsonPropertyName("device_model")]
+                        public string? DeviceModel { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置 Windows 设备信息。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("windows")]
+                        [System.Text.Json.Serialization.JsonPropertyName("windows")]
+                        public Types.WindowsDevice? WindowsDevice { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置 Mac 设备信息。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("mac")]
+                        [System.Text.Json.Serialization.JsonPropertyName("mac")]
+                        public Types.MacDevice? MacDevice { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置移动设备信息。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("mobile")]
+                        [System.Text.Json.Serialization.JsonPropertyName("mobile")]
+                        public Types.MobileDevice? MobileDevice { get; set; }
+                    }
+                }
+
                 /// <summary>
                 /// 获取或设置操作者 UserId。
                 /// </summary>
@@ -22,6 +138,13 @@ namespace SKIT.FlurlHttpClient.Wechat.Work.Models
                 [Newtonsoft.Json.JsonProperty("department_id")]
                 [System.Text.Json.Serialization.JsonPropertyName("department_id")]
                 public long DepartmentId { get; set; }
+
+                /// <summary>
+                /// 获取或设置截屏操作类型。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("operation_type")]
+                [System.Text.Json.Serialization.JsonPropertyName("operation_type")]
+                public int OperationType { get; set; }
 
                 /// <summary>
                 /// 获取或设置截屏内容类型。
@@ -50,6 +173,20 @@ namespace SKIT.FlurlHttpClient.Wechat.Work.Models
                 [Newtonsoft.Json.JsonProperty("system")]
                 [System.Text.Json.Serialization.JsonPropertyName("system")]
                 public string? System { get; set; }
+
+                /// <summary>
+                /// 获取或设置设备 IP。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("ip")]
+                [System.Text.Json.Serialization.JsonPropertyName("ip")]
+                public string? Ip { get; set; }
+
+                /// <summary>
+                /// 获取或设置设备信息。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("device_info")]
+                [System.Text.Json.Serialization.JsonPropertyName("device_info")]
+                public Types.DeviceInfo? DeviceInfo { get; set; }
             }
         }
 
